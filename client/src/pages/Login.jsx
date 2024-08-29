4178cc2a38858af6bbc5f89708ec009ba09f4cd3
RahulMoreNttdata <Rahul.More@nttdata.com>
2024-08-29 18:17:30 +0530

    changes

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -5,7 +5,7 @@ const Login = () => {
   const [error, setError] = useState(null);
   const login = (data) => {
     console.log("login", data);
-    setError("Hello")
+    setError("Hello login")
   };
   
   return <Auth isSignup={false} loginOrRegister={login} serverError={error} />;
